0c7e5071db9f4d2b9b5f201bb63696c3c9b623d1
Norman Manantan <norman@flags.fm>
2020-07-06 12:13:24 +0800

    Display component

---
+++ src/components/Display/index.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react';
+import { display } from './style.module.scss';
+
+export default function Display():ReactElement {
+  return (
+    <div className={display}>1</div>  
+  );
+};
